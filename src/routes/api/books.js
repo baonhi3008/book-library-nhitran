c0b1b689b45b3c30aaf685ee9388bd4af82075fb
Tran Dang Bao Nhi <tr.baonhi3008@gmail.com>
2023-03-12 00:02:59 +0700

    add middleware to route

--- src/routes/api/books.js
+++ src/routes/api/books.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const validatorSchema = require('../../schema/book')
 const bookController = require('../../controllers/books');
-const {handleErrors} = require('../../middleware/errorHandler.middleware')
+const errorHandler = require('../../middleware/errorHandler.middleware')
+const validator = require('../../middleware/validation');
+const { queryForAuthor, paramBookById, patchIsLiked } = require('../../schema/book');
 const router = express.Router();
 
 // GET localhost:8080/allbooks
-router.get('/allbooks', bookController.handleGet);
+router.get('/allbooks',validator.generateValidationMiddleware(queryForAuthor,'query'),bookController.handleGet);
 router.post('/allbooks',bookController.storeData)
-router.get('/allbooks/:id',bookController.getOneBook)
-router.patch('/allbooks/:id',bookController.updateLike)
+router.get('/allbooks/:id',validator.generateValidationMiddleware(paramBookById,'params'),bookController.getOneBook)
+router.patch('/allbooks/:id',validator.generateValidationMiddleware(paramBookById,'params'),validator.generateValidationMiddleware(patchIsLiked,'body'),bookController.updateLike)
 router.get('/allbooks',bookController.sortBook)
 // router.get('/allbooks?:author',bookController.getBooksFromAuthor)
 
